f78706aaac13c103d909a947fb0796a303d23bf4
dikiogres <dikiogres@gmail.com>
2022-11-02 00:15:44 +0700

    feat: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,19 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent){
-    if(req.nextUrl.pathname.startsWith("api/get-url")){
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const slug = req.nextUrl.pathname.split("/").pop();
 
-        console.log("returning early")
+  const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
+  if (slugFetch.status === 404) {
+    return NextResponse.redirect(req.nextUrl.origin);
+  }
+  const data = await slugFetch.json();
 
-        return;
-    }
-
-    const slug = req.nextUrl.pathname.split("/").pop();
-
-    const data = await (
-       await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)
-    ).json();
-
-    if(data?.url){
-        return NextResponse.redirect(data.url);
-    }
-
-
+  if (data?.url) {
+    return NextResponse.redirect(data.url);
+  }
 }
 
+export const config = {
+  matcher: "/:slug",
+};
