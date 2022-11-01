dc1a4f84ce9645203173176702fa2c307e18a37a
dikiogres <dikiogres@gmail.com>
2022-11-02 00:39:34 +0700

    feat:  lint formatting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { withTRPC } from "@trpc/next";
+import { AppRouter } from "./api/trpc/[trpc]";
 
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+
+function getBaseUrl() {
+  if (process.browser) return ""; // Browser should use current path
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+}
+
+export default withTRPC<AppRouter>({ 
+  
+  config() {
+    const url = `${getBaseUrl()}/api/trpc`;
+
+    return {
+      url,
+    };
+  },
+  ssr: false,
+
+})(MyApp);
